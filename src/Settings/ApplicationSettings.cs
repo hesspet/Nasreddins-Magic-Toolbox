214a9e05652e8067c4d1b393c2254deb4cf09427
hesspet@gmail.com <hesspet@gmail.com>
2025-10-12 14:59:42 +0200

    Datemtyp geändert

--- src/Settings/ApplicationSettings.cs
+++ src/Settings/ApplicationSettings.cs
@@ -1,12 +1,11 @@
 namespace Toolbox.Settings;
 
 /// <summary>
-/// Provides strongly typed information about application settings stored in local storage.
+///     Provides strongly typed information about application settings stored in local storage.
 /// </summary>
 public static class ApplicationSettings
 {
-    public const string SplashScreenDurationKey = "SplashScreenDurationSeconds";
     public const int SplashScreenDurationDefaultSeconds = 5;
-
-    public static int[] SplashScreenDurationOptions { get; } = new[] { 0, 5, 10, 60 };
+    public const string SplashScreenDurationKey = "SplashScreenDurationSeconds";
+    public static int[] SplashScreenDurationOptions { get; } = [0, 5, 10, 60];
 }
