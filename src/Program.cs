8967d34b878a18fabc931807258eba18a6364e62
Nasr ad-Din al-Quffaz and Peter Heß (DE) <hesspet@gmx.de>
2025-10-13 16:27:59 +0200

    Add startup bootstrap for built-in tarot deck

--- src/Program.cs
+++ src/Program.cs
@@ -15,5 +15,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<LocalStorageHelper>();
 builder.Services.AddScoped<IndexedDbHelper>();
+builder.Services.AddScoped<DeckBootstrapper>();
 
 await builder.Build().RunAsync();
